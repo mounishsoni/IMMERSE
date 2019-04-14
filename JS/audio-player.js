acbf3d4fe3a1203126efe4ba45f49ee0f3621975
mounishsoni <47580951+mounishsoni@users.noreply.github.com>
2019-04-14 19:54:07 -0400

    weekly songs done

--- JS/audio-player.js
+++ JS/audio-player.js
@@ -1,7 +1,10 @@
 audioPlayer();
+audioPlayer2();
 
 function audioPlayer(){
-	$("#audioPlayer")[0].src = $(".card-body h5 a")[0];
+	
+	var weekly = $(".card-body h5 a")[0];
+	$("#audioPlayer")[0].src = weekly;
 	$("#audioPlayer")[0].play();
 	$(".card-body h5 a").click(function(e){
 		e.preventDefault();
