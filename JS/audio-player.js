82443e5ad350b32aa7ed9461e55a43b46d0e858c
mounishsoni <47580951+mounishsoni@users.noreply.github.com>
2019-04-14 20:09:31 -0400

    adjust seeking bar & ibox

--- JS/audio-player.js
+++ JS/audio-player.js
@@ -1,14 +1,19 @@
 audioPlayer();
-audioPlayer2();
 
 function audioPlayer(){
 	
 	var weekly = $(".card-body h5 a")[0];
 	$("#audioPlayer")[0].src = weekly;
-	$("#audioPlayer")[0].play();
 	$(".card-body h5 a").click(function(e){
 		e.preventDefault();
 		$("#audioPlayer")[0].src = this;
 		$("#audioPlayer")[0].play();
 	});
+}
+
+immerseBox();
+
+function immerseBox () {
+
+	$("#immerseBox")[0].src = $(".card-body h5 a")[0];
 }
